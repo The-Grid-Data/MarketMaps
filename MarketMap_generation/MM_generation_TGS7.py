d3bbee2a9a7f9b1a837041eae9b2721ce6b0a650
Hugo Moura <hugomoura02@gmail.com>
2024-12-13 12:47:39 +0100

    rename

--- MarketMap_generation/MM_generation_TGS7.py
+++ MarketMap_generation/MM_generation_TGS7.py
@@ -2,8 +2,8 @@ import requests
 import json
 import traceback
 
-from MM_preparation.data_processor import process_data
-from MM_preparation.helpers import generate_results_content, generate_csv_content, create_zip_file, create_sector_based_output, filter_by_sector
+from MarketMap_generation.data_processor import process_data
+from MarketMap_generation.helpers import generate_results_content, generate_csv_content, create_zip_file, create_sector_based_output, filter_by_sector
 
 url = "https://beta.node.thegrid.id/graphql"
 
